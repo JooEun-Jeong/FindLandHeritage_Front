4a2a2f49c04f809dba834b25e0aecf5ec6d03a06
JooEun-Jeong <prichard147@naver.com>
2023-06-26 15:38:27 +0900

    [chore] prepare for tabs by separating navigators

--- App.js
+++ App.js
@@ -7,22 +7,51 @@ import MyPageScreen from './screens/MyPageScreen';
 import LoginScreen from './screens/LoginScreen';
 // screen changes
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 // for redux: state controll
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+// bottom navigator
+
+const Stack = createNativeStackNavigator();
+const StackNavi = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* <Stack.Screen name="main" component={MainScreen} /> */}
+      {/* <Stack.Screen name="login" component={LoginScreen} /> */}
+      <Stack.Screen name="description" component={DescriptionScreen} />
+      <Stack.Screen name="search" component={SearchScreen} />
+      <Stack.Screen name="mypage" component={MyPageScreen} />
+    </Stack.Navigator>
+  )
+}
+
+
+const Tab = createBottomTabNavigator();
+
+const TabNavi = ({ navigation }) => {
+  return (
+    <NavigationContainer
+      screenOptions={({ route }) => ({ headerShown: false })}
+      independent={true}
+    >
+      <Tab.Navigator initialRouteName="description">
+        <Tab.Screen name="description" component={DescriptionScreen} />
+        <Tab.Screen name="search" component={SearchScreen} />
+        <Tab.Screen name="mypage" component={MyPageScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="main" component={MainScreen} /> */}
-          {/* <Stack.Screen name="login" component={LoginScreen} /> */}
-          <Stack.Screen name="description" component={DescriptionScreen} />
-          <Stack.Screen name="search" component={SearchScreen} />
-          <Stack.Screen name="mypage" component={MyPageScreen} />
+      <NavigationContainer independent={true}>
+        <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='stackNavi'>
+          <Stack.Screen name="tabnavi" component={TabNavi} />
+          <Stack.Screen name='stackNavi' component={StackNavi} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
